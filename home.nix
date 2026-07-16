b377b90feb0547148df831160d6c02dfa61421e3
Andreas <andreas.m.weber@pm.me>
2026-07-16 16:30:10 +0200

    Add  Home Manager

---
+++ home.nix
@@ -0,0 +1,10 @@
+{config, pkgs, ...}:
+
+{
+    home.username = "hrolgard";
+    home.homeDirectory = "/home/hrolgard";
+
+    home.stateVersion = "26.05";
+
+    programs.home-manager.enable = true;
+}
