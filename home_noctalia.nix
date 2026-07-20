7d67d382b6b6b1a921e078c90ed5e64844253597
Andreas <andreas.m.weber@pm.me>
2026-07-20 02:55:20 +0200

    Configured Noctalia some more

--- home_noctalia.nix
+++ home_noctalia.nix
@@ -8,7 +8,26 @@
     programs.noctalia = {
         enable = true;
         settings = {
-            bar.main.position = "bottom";
+            bar.main = {
+                position = "bottom";
+                margin_edge = 0;
+                reserve_space = false;
+                smart_auto_hide = true;
+                show_on_workspace_switch = true;
+                thickness = 55;
+
+
+                start  = [
+                    "launcher"
+                    "workspaces"
+                ];
+                center = [
+                    "clock"
+                ];
+                end    = [
+                    "tray" "notifications" "clipboard" "network" "bluetooth" "volume" "brightness" "battery" "control-center" "session"
+                ];
+            };
         };
     };
 }
