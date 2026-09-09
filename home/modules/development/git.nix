52aa82ff71a97a608b899c464d832623d6977e36
Hrolgard <andreas.m.weber@pm.me>
2026-09-09 21:40:09 +0200

    took care of 2 deprecation warnings

--- home/modules/development/git.nix
+++ home/modules/development/git.nix
@@ -1,13 +1,19 @@
 {...}: {
     programs.git = {
         enable = true;
-        settings.user = {
-            name = "Hrolgard";
-            email = "andreas.m.weber@pm.me";
+
+        settings = {
+            user = {
+                name = "Hrolgard";
+                email = "andreas.m.weber@pm.me";
+            };
+
+            alias = {
+                acp = "!f() { git add . && git commit -m \"$1\" && git push; }; f";
+            };
         };
 
-        aliases = {
-            acp = "!f() { git add . && git commit -m \"$1\" && git push; }; f";
-        };
+
+
     };
 }
--- home/modules/development/vsc/vsc.nix
+++ home/modules/development/vsc/vsc.nix
@@ -1,7 +1,7 @@
 {pkgs, ...}: {
     programs.vscode = {
         enable = true;
-        extensions = with pkgs.vscode-extensions; [
+        profiles.default.extensions = with pkgs.vscode-extensions; [
             esbenp.prettier-vscode
             oderwat.indent-rainbow
             zainchen.json
