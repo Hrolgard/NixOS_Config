019e4c3a52fd72704146c008ab8df340e9652009
Hrolgard <andreas.m.weber@pm.me>
2026-09-09 20:53:51 +0200

    removed any extensions from decleration for now

--- home/modules/development/vsc.nix
+++ home/modules/development/vsc.nix
@@ -1,9 +1,6 @@
 {...}: {
     programs.vscode = {
         enable = true;
-        profiles.default.extensions = with pkgs.vscode-extensions; [
-            dracula-theme.theme-dracula
-        ];
     };
 }
 
