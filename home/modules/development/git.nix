92d4ab707c7587abce20b0b4653727160d24bf15
Hrolgard <andreas.m.weber@pm.me>
2026-09-09 21:32:52 +0200

    added git acp alias

--- home/modules/development/git.nix
+++ home/modules/development/git.nix
@@ -3,7 +3,11 @@
         enable = true;
         settings.user = {
             name = "Hrolgard";
-            email = "andreas.m.weber@pm.me ";
+            email = "andreas.m.weber@pm.me";
+        };
+
+        aliases = {
+            acp = "!f() { git add . && git commit -m \"$1\" && git push; }; f";
         };
     };
 }
