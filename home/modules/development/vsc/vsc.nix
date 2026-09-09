bc5bb98fd184a85675756bbef77ecef4ac832559
Hrolgard <andreas.m.weber@pm.me>
2026-09-09 21:36:15 +0200

    removed git lens from vsc. Anything that starts with an ad is not welcome in my dev environment

--- home/modules/development/vsc/vsc.nix
+++ home/modules/development/vsc/vsc.nix
@@ -2,7 +2,6 @@
     programs.vscode = {
         enable = true;
         extensions = with pkgs.vscode-extensions; [
-            eamodio.gitlens
             esbenp.prettier-vscode
             oderwat.indent-rainbow
             zainchen.json
